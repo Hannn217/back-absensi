31a61244bba8ceea13e800b47164762318569e7c
Hannn217 <bongtzehan@gmail.com>
2024-10-29 10:28:42 +0700

    pe

--- routes/api.php
+++ routes/api.php
@@ -81,7 +81,7 @@ Route::middleware(['auth:sanctum', 'KetuaKelasMiddleware', 'cuti.pegawai'])->gro
 // Route untuk Pegawai
 Route::middleware(['auth:sanctum', 'pegawai', 'PegawaiMiddleware'])->group(function () {
     Route::get('/profile/{username}', [PegawaiController::class, 'profile']); //get profil ketua kelas
-    Route::post('/pegawai/absen', [PegawaiController::class, 'store']); //untuk absen
+    Route::post('/pegawai/absen/{username}', [PegawaiController::class, 'store']); //untuk absen
     Route::delete('/pegawai/absen/delete/{id}', [PegawaiController::class, 'destroy']); //untuk hapus absen
     Route::post('/pengajuan', [PengajuanCutiController::class, 'pengajuan']); //untuk mengajukan cuti 
     Route::post('/pegawai/logout', [PegawaiController::class, 'logout']); //untuk logout
